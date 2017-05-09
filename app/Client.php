deea2fa29205365ecd3257a5641b106377fa2f98
RachaelMack <rachael.mackeigan@gmail.com>
2017-05-09 14:56:39 -0300

    custom controllers

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-  //
+  public function organizationId(){
+  return $this->belongsTo(Organization::class);
+}
 }
--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,5 +7,4 @@ use Illuminate\Http\Request;
 class ClientController extends Controller
 {
     //
-    
 }
