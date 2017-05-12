97efbea70376251d600c8a99c5c0c078b6875034
RachaelMack <rachael.mackeigan@gmail.com>
2017-05-12 11:59:26 -0300

    implement iSeed and updated permissions for users and managers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(VoyagerDatabaseSeeder::class);
         $this->call(VoyagerDummyDatabaseSeeder::class);
 
+        $this->call(PermissionRoleTableSeeder::class);
     }
 }
